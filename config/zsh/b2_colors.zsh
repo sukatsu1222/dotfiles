a57d695d0035e03edd7e646a006f8bce4476b47e
sukatsu1222 <sukatsu1222@gmail.com>
2018-12-22 18:33:37 +0900

    Delete zsh directory

--- config/zsh/b2_colors.zsh
+++
@@ -1,4 +0,0 @@
-# zsh/b2_colors.zsh
-if [ -x /usr/bin/dircolors ]; then
-  test -r ${HOME}/.dircolors && eval "$(dircolors -b ${HOME}/.dircolors)" || eval "$(dircolors -b)"
-fi
--- config/zsh/z2_sources.zsh
+++
@@ -1,8 +0,0 @@
-# zsh/z2_sources.zsh
-if type kubectl >/dev/null 2>&1; then
-  source <(kubectl completion zsh)
-fi
-
-if type minikube >/dev/null 2>&1; then
-  source <(minikube completion zsh)
-fi
