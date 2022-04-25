23acf7d4c6bd55ad315205dc8e4852f21a899a9b
sukatsu1222 <sukatsu1222@gmail.com>
2022-04-25 22:04:13 +0900

    Update install script

--- install.sh
+++ install.sh
@@ -2,11 +2,5 @@
 
 DOT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}) && pwd)
 
-# Install zimfw
-if [[ ! -e ${HOME}/.zim/zimfw.zsh ]]; then
-  curl -Lo ${HOME}/.zim/zimfw.zsh --create-dirs https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
-fi
-
 source ${DOT_DIR}/deploy.sh
 
-source ${HOME}/.zim/zimfw.zsh install
