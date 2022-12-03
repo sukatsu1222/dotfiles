9c8ba061029e151e3dcc8f3a0017e5dbd8b4810c
sukatsu1222 <sukatsu1222@gmail.com>
2022-12-03 09:33:39 +0900

    Update nvim

--- install.sh
+++ install.sh
@@ -4,3 +4,11 @@ DOT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}) && pwd)
 
 source ${DOT_DIR}/deploy.sh
 
+if (( ${+commands[nvim]} )) && [[ ! -s ${HOME}/.local/share/nvim/site/autoload/plug.vim ]]; then
+    curl -fLo ${HOME}/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    nvim -es -u $HOME/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "q"
+fi
+
+echo ""
+echo "Installation Finished"
