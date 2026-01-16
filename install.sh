fa128a072d208a0ec176776d0ed9c1d2b7a34a00
sukatsu1222 <sukatsu1222@gmail.com>
2026-01-16 12:18:53 +0900

    Add TPM and zimfw installation to install.sh

--- install.sh
+++ install.sh
@@ -4,10 +4,24 @@ DOT_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}) && pwd)
 
 source ${DOT_DIR}/deploy.sh
 
-if (( ${+commands[nvim]} )) && [[ ! -s ${HOME}/.local/share/nvim/site/autoload/plug.vim ]]; then
-    curl -fLo ${HOME}/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    nvim -es -u $HOME/.config/nvim/init.vim -i NONE -c "PlugInstall" -c "q"
+# Install TPM (Tmux Plugin Manager) on Ubuntu
+if [[ "$(uname -s)" == "Linux" ]]; then
+  TPM_DIR="${HOME}/.config/tmux/plugins/tpm"
+  if [[ ! -d "${TPM_DIR}" ]]; then
+    echo "Installing TPM (Tmux Plugin Manager)..."
+    git clone https://github.com/tmux-plugins/tpm "${TPM_DIR}"
+  else
+    echo "TPM is already installed."
+  fi
+fi
+
+# Install zimfw (Zim - Zsh IMproved)
+ZIM_HOME="${HOME}/.config/zsh/.zim"
+if [[ ! -d "${ZIM_HOME}" ]]; then
+  echo "Installing zimfw..."
+  curl -fsSL https://raw.githubusercontent.com/zimfw/install/master/install.zsh | ZIM_HOME="${ZIM_HOME}" zsh
+else
+  echo "zimfw is already installed."
 fi
 
 echo ""
