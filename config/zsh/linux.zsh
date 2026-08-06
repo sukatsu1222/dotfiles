5849df678b33251679968cc3cb72930e5df1ebc4
sukatsu1222 <sukatsu1222@gmail.com>
2026-08-06 15:40:08 +0900

    Remove tmux auto-start on login in linux.zsh

--- config/zsh/linux.zsh
+++ config/zsh/linux.zsh
@@ -4,14 +4,3 @@
 
 # Set unlimited stack size for AC Library (C++ competitive programming)
 ulimit -s unlimited
-
-# Auto-start tmux on login
-# Attach to existing 'default' session if present (silent check to avoid
-# racing with tmux-continuum auto-restore), otherwise create a new one.
-if [[ -o login ]] && (( ${+commands[tmux]} )) && [[ -z "${TMUX}" ]]; then
-  if tmux has-session -t default 2>/dev/null; then
-    tmux attach -t default
-  else
-    tmux new -s default
-  fi
-fi
